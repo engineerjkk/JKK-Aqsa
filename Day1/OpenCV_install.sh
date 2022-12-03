050c8b5fc2a50c51b7f9f767b069a96001ace95f
JuneKoo Kang <engineerjkk@naver.com>
2022-12-04 02:36:21 +0900

    Add files via upload

---
+++ Day1/OpenCV_install.sh
@@ -0,0 +1,59 @@
+pkg-config --modversion opencv
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev
+sudo apt-get install -y libv4l-dev v4l-utils
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y libqt4-dev
+sudo apt-get install -y libatlas-base-dev gfortran libeigen3-dev
+sudo apt-get install -y python2.7-dev python3-dev python-numpy python3-numpy
+
+
+mkdir opencv
+d opencv
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.1.zip
+unzip opencv.zip
+
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.1.zip
+unzip opencv_contrib.zip
+
+
+cd opencv-4.0.1/
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D WITH_TBB=OFF \
+-D WITH_IPP=OFF \
+-D WITH_1394=OFF \
+-D BUILD_WITH_DEBUG_INFO=OFF \
+-D BUILD_DOCS=OFF \
+-D INSTALL_C_EXAMPLES=ON \
+-D INSTALL_PYTHON_EXAMPLES=ON \
+-D BUILD_EXAMPLES=OFF \
+-D BUILD_TESTS=OFF \
+-D BUILD_PERF_TESTS=OFF \
+-D WITH_QT=ON \
+-D WITH_GTK=OFF \
+-D WITH_OPENGL=ON \
+-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.0.1/modules \
+-D WITH_V4L=ON  \
+-D WITH_FFMPEG=ON \
+-D WITH_XINE=ON \
+-D BUILD_NEW_PYTHON_SUPPORT=ON \
+-D OPENCV_GENERATE_PKGCONFIG=ON ../
+cat /proc/cpuinfo | grep processor | wc -l
+
+time make -j4
+sudo make install
+
+
+cat /etc/ld.so.conf.d/*
+sudo sh -c 'echo '/usr/local/lib' > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
---
+++ Day1/README.md
@@ -0,0 +1,4 @@
+1일차 수업 코드
+
+차선 인식 실습을위한 input data
+  https://drive.google.com/file/d/1dy75eQLC-wCH34R1sYD8lfOIVsHLG61Q/view?usp=sharing
